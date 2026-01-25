5484e40c66ac1a102cf2bcd1a4b9d4d5eb0b4308
Asifur Rahman <asifurrahman.ac@gmail.com>
2026-01-26 00:38:46 +0600

    table text centered

--- src/components/page-layout/dashboard-page/Revenue.jsx
+++ src/components/page-layout/dashboard-page/Revenue.jsx
@@ -39,12 +39,16 @@ const Revenue = () => {
                 {revenueData?.map((product, index) => {
                   return (
                     <tr key={index}>
-                      <th>{index + 1}</th>
+                      <th className="text-center">{index + 1}</th>
                       <td>{product?.title}</td>
-                      <td>{product?.company}</td>
-                      <td>{product?.total_users}</td>
-                      <td>{product?.revenue_estimate?.avg_price}</td>
-                      <td>{product?.revenue_estimate?.total_bdt}</td>
+                      <td className="text-center">{product?.company}</td>
+                      <td className="text-center">{product?.total_users}</td>
+                      <td className="text-center">
+                        {product?.revenue_estimate?.avg_price}
+                      </td>
+                      <td className="text-center">
+                        {product?.revenue_estimate?.total_bdt}
+                      </td>
                       <td>{product?.category}</td>
                       <td>{product?.subcategory}</td>
                     </tr>

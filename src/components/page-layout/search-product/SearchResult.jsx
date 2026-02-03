cba3c5e638025854301e68d5dd5c1bfa087eeaec
Asifur Rahman <asifurrahman.ac@gmail.com>
2026-02-03 23:50:00 +0600

    search result component ui grid updated

--- src/components/page-layout/search-product/SearchResult.jsx
+++ src/components/page-layout/search-product/SearchResult.jsx
@@ -9,8 +9,8 @@ const SearchResult = ({ productData }) => {
   }, [productData]);
   return (
     <section className="lg:w-4/5 mx-auto my-5">
-      <div className="w-full flex justify-between items-center my-4">
-        <div className="w-full grid lg:grid-cols-4 md:grid-cols-3 smd:grid-cols-2 grid-cols-1 place-content-center">
+      <div className="w-full flex justify-between items-center my-4 mx-auto">
+        <div className="w-full grid lg:grid-cols-4 md:grid-cols-3 smd:grid-cols-2 grid-cols-1 place-items-center">
           {products?.map((product, index) => {
             return <ProductCard product={product} key={index}></ProductCard>;
           })}

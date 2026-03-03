d67e4df010b99c7bce8346c324e04109fd2b5def
Asifur Rahman <asifurrahman.ac@gmail.com>
2026-03-03 22:39:52 +0600

    search reslut component created

--- src/app/search/[keyword]/page.jsx
+++ src/app/search/[keyword]/page.jsx
@@ -1,15 +1,26 @@
+import SearchResult from "@/components/page-layout/search-product/SearchResult";
+import { Suspense } from "react";
 
-const page = async({params}) => {
-    const {keyword} = await params
-    const decodedURI = decodeURIComponent(keyword)
-    const res = await fetch(`${process.env.NEXT_BACKEND_URL}/search/${encodeURIComponent(
-      decodedURI
-    )}`)
-    return (
-        <div>
-            {decodedURI}
-        </div>
-    );
+const page = async ({ params }) => {
+  const { keyword } = await params;
+  const decodedURI = decodeURIComponent(keyword);
+  const res = await fetch(
+    `${process.env.NEXT_BACKEND_URL}/search/${encodeURIComponent(decodedURI)}`,
+  );
+  const data = await res.json();
+  return (
+    <div className="w-full">
+      <Suspense
+        fallback={
+          <div className="flex justify-center items-center w-full min-h-screen">
+            <span className="loading loading-spinner loading-xl"></span>
+          </div>
+        }
+      >
+        <SearchResult products={data}></SearchResult>
+      </Suspense>
+    </div>
+  );
 };
 
 export default page;

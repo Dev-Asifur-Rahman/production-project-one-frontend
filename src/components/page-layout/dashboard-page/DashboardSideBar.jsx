e22cd091f0c40de7b04e4b2ef4840c37bb00b9b4
Asifur Rahman <asifurrahman.ac@gmail.com>
2026-08-16 20:26:43 +0600

    menu button aligned center in sm screen

--- src/components/page-layout/dashboard-page/DashboardSideBar.jsx
+++ src/components/page-layout/dashboard-page/DashboardSideBar.jsx
@@ -6,7 +6,7 @@ import CategoryPage from "./CategoryPage";
 
 import AllProducts from "./AllProducts";
 import ClickedInfo from "./ClickedInfo";
-import Leaderboard from './Leaderboard';
+import Leaderboard from "./Leaderboard";
 import RisingStars from "./RisingStars";
 import { LanguageContext } from "@/context/GlobalLanguageProvider";
 import translation from "@/utils/translation";
@@ -14,7 +14,7 @@ import Revenue from "./Revenue";
 
 const DashboardSideBar = () => {
   const [tab, setTab] = useState("item1");
-  const {lan} = useContext(LanguageContext)
+  const { lan } = useContext(LanguageContext);
 
   const handleClick = (newTab) => {
     setTab(newTab);
@@ -31,9 +31,14 @@ const DashboardSideBar = () => {
     <div className="drawer md:drawer-open">
       <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content">
-        <label htmlFor="my-drawer-3" className="btn drawer-button md:hidden">
-          {translation[lan].dashboard.menu.heading}
-        </label>
+        <div className="w-fit mx-auto">
+          <label
+            htmlFor="my-drawer-3"
+            className="btn drawer-button md:hidden bg-dealbondhu text-white"
+          >
+            {translation[lan].dashboard.menu.heading}
+          </label>
+        </div>
 
         {/* Right-side content */}
         <div className="p-4 w-full">
@@ -52,29 +57,45 @@ const DashboardSideBar = () => {
         <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
         <ul className="menu bg-base-200 min-h-full w-60 p-4">
           <li>
-            <a onClick={() => handleClick("item1")}>{translation[lan].dashboard.categories.heading}</a>
+            <a onClick={() => handleClick("item1")}>
+              {translation[lan].dashboard.categories.heading}
+            </a>
           </li>
           <li>
-            <a onClick={() => handleClick("item2")}>{translation[lan].dashboard.products.heading}</a>
+            <a onClick={() => handleClick("item2")}>
+              {translation[lan].dashboard.products.heading}
+            </a>
           </li>
           <li>
-            <a onClick={() => handleClick("item3")}>{translation[lan].dashboard.pendingProducts.heading}</a>
+            <a onClick={() => handleClick("item3")}>
+              {translation[lan].dashboard.pendingProducts.heading}
+            </a>
           </li>
           <li>
-            <a onClick={() => handleClick("item4")}>{translation[lan].dashboard.archiveProducts.heading}</a>
+            <a onClick={() => handleClick("item4")}>
+              {translation[lan].dashboard.archiveProducts.heading}
+            </a>
           </li>
-          
+
           <li>
-            <a onClick={() => handleClick("item6")}>{translation[lan].dashboard.clickedInfo.heading}</a>
-          </li>
-          <li>
-            <a onClick={() => handleClick("item7")}>{translation[lan].dashboard.risingStars.heading}</a>
-          </li>
-          <li>
-            <a onClick={() => handleClick("item8")}>{translation[lan].dashboard.leaderboard.heading}</a>
+            <a onClick={() => handleClick("item6")}>
+              {translation[lan].dashboard.clickedInfo.heading}
+            </a>
           </li>
           <li>
-            <a onClick={() => handleClick("item9")}>{translation[lan].dashboard.revenue.heading}</a>
+            <a onClick={() => handleClick("item7")}>
+              {translation[lan].dashboard.risingStars.heading}
+            </a>
+          </li>
+          <li>
+            <a onClick={() => handleClick("item8")}>
+              {translation[lan].dashboard.leaderboard.heading}
+            </a>
+          </li>
+          <li>
+            <a onClick={() => handleClick("item9")}>
+              {translation[lan].dashboard.revenue.heading}
+            </a>
           </li>
         </ul>
       </div>
